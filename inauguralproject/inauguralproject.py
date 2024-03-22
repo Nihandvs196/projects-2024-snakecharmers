05706afaf6a40561278fa7097716c2b46c525f94
Serkan Korkmaz <77464572+serkor1@users.noreply.github.com>
2024-03-23 00:33:44 +0100

    Finished Inaugrual Project (#1) (#2)
    * Preliminairy Work
    
    In this commit Question 1 part 1 has been answered. The edgeworth box needs to be optimized.
    
    In the next commit it will be made as a function.
    
    * Updated Jupyter-Notebook (Errors, New Answers, etc)
    
    Minor Errors Found in Notebook:
    
    1. The good have been changed from N = 76, to N = 75.
    2. The initial endowments are the reciprocal of each other, so these
    are to represent 1 point on the Edgewort box - not 2. Awkward.
    3. Added Solution to Question 5 a + b
    
    The Notebook needs to be heavily optimized and cleaned up. It is insanely messy at the current stage.
    
    * # Commit from Pycharm
    
    This commit is a forced commit from pyCharm - without it doesnt allow to switch branches.
    
    Completed Question 6.a. Next commit 6b, 7 and 8.
    
    * # Commit from Pycharm
    
    This commit is a forced commit from pyCharm - without it doesnt allow to switch branches.
    
    Completed Question 6.a. Next commit 6b, 7 and 8.
    
    Question 6b, 7 and 8 is answered.
    
    NOTE: I am not sure how pycharms git extension works.
    
    * # Added Requirements.txt
    
    This file should make it easier to identfy all dependencies.

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,15 +1,45 @@
-def square(x):
-    """ square numpy array
-    
-    Args:
-    
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
-    
+# create a demand function
+# such that it can be used
+import numpy as np
+
+class demand:
     """
-    
-    y = x**2
-    return y
+    The class function returns the demand function
+    for each consumer.
+    The fraction determines the alpha and beta of the
+    consumer, while the good determines wether
+    its good 1 or 2
+    """
+
+    def __init__(self, fraction, good):
+        self.fraction = 1 - fraction if good == 2 else fraction
+        self.good = good
+
+    def function(self, good, price):
+        """
+        The function returns the demand for the chosen
+        good in the first the defined class.
+        Args:
+            good: A list of integers representing the demand for the good
+            defined initially
+            price: A list of intergers representing the price for each
+            good.
+
+        Returns:
+            The total demand for the good 1 or 2
+
+        """
+        # 1) coerce good and price
+        # to numpy arrays
+        good = np.array(good)
+        price = np.array(price)
+
+        demand = self.fraction * sum(price * good) / price[self.good - 1]
+
+        return np.maximum(demand, 0)
+
+def utility(fraction, good):
+    good_1 = np.array(np.maximum(good[0], 0)) ** fraction
+    good_2 = (np.array(np.maximum(good[1], 0))) ** (1-fraction)
+
+    return good_1 * good_2
